2dafb0e4ed806d4aed514cf68eeb6b270971d24d
JiahaoHong1997 <hongjiahao1997@gmail.com>
2021-12-25 22:39:31 +0800

    lru

---
+++ 1.lru/lru.go
@@ -0,0 +1,78 @@
+package lru
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type Cache struct {
+	maxBytes	int64
+	nBytes 		int64
+	ll 			*list.List
+	cache 		map[string]*list.Element
+	onEvicted 	func(key string, value Value)
+}
+
+type entry struct {
+	key 	string
+	value 	Value
+}
+
+type Value interface {
+	Len()	int
+}
+
+// Len function for Cache class, the other Len function for value need to implement by user,
+// the example is shown in /1.lru/lru_test.go
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes: maxBytes,
+		ll: list.New(),
+		cache: make(map[string]*list.Element),
+		onEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// RemoveOldest removes the oldest item
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+	}
+}
+
+// Add adds a value to the cache
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key: key, value: value})
+		c.cache[key] = ele
+		c.nBytes += int64(value.Len()) + int64(len(key))
+
+	}
+
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+	fmt.Println(c.nBytes)
+}
